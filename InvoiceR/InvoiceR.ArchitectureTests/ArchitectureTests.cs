33e791530b40ac3858355cc39c70863decabc4a9
patryk <patryksladek@wp.pl>
2024-03-14 21:14:57 +0100

    Added architecture tests

---
+++ InvoiceR/InvoiceR.ArchitectureTests/ArchitectureTests.cs
@@ -0,0 +1,109 @@
+﻿using FluentAssertions;
+using NetArchTest.Rules;
+using Xunit;
+
+namespace InvoiceR.ArchitectureTests;
+
+public class ArchitectureTests
+{
+    private const string DomainNamespace = "InvoiceR.Domain";
+    private const string ApplicationNamespace = "InvoiceR.Application";
+    private const string InfrastructureNamespace = "InvoiceR.Infrastructure";
+    private const string PresentationNamespace = "InvoiceR.Presentation";
+    private const string ApiNamespace = "InvoiceR.WebAPI";
+
+    [Fact]
+    public void Domain_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        // Arrange
+        var assembly = typeof(Domain.Entities.BaseEntity).Assembly;
+
+        var otherProjects = new[]
+        {
+            ApplicationNamespace,
+            InfrastructureNamespace,
+            PresentationNamespace,
+            ApiNamespace
+        };
+
+        // Act
+        var testResult = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        // Assert
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Application_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        // Arrange
+        var assembly = typeof(Application.Extensions).Assembly;
+
+        var otherProjects = new[]
+        {
+            InfrastructureNamespace,
+            PresentationNamespace,
+            ApiNamespace
+        };
+
+        // Act
+        var testResult = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        // Assert
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Infrastruture_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        // Arrange
+        var assembly = typeof(Infrastructure.Extensions).Assembly;
+
+        var otherProjects = new[]
+        {
+            PresentationNamespace,
+            ApiNamespace
+        };
+
+        // Act
+        var testResult = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        // Assert
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Presentation_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        // Arrange
+        var assembly = typeof(Presentation.Extensions).Assembly;
+
+        var otherProjects = new[]
+        {
+            InfrastructureNamespace,
+            ApiNamespace
+        };
+
+        // Act
+        var testResult = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        // Assert
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+}
