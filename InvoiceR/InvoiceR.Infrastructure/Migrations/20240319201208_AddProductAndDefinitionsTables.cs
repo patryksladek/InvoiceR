8b02087e9ad05fcd9dc953bb92220dd413a7131a
patryk <patryksladek@wp.pl>
2024-03-19 21:38:16 +0100

    Added Product and Definitions tables migration

---
+++ InvoiceR/InvoiceR.Infrastructure/Migrations/20240319201208_AddProductAndDefinitionsTables.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceR.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductAndDefinitionsTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "definitions");
+
+            migrationBuilder.EnsureSchema(
+                name: "products");
+
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                schema: "definitions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Symbol = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", precision: 240, nullable: false),
+                    IsDefault = table.Column<bool>(type: "bit", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VatRates",
+                schema: "definitions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Symbol = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false),
+                    Value = table.Column<decimal>(type: "decimal(2,2)", precision: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VatRates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                schema: "products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(240)", maxLength: 240, nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Barcode = table.Column<string>(type: "nvarchar(130)", maxLength: 130, nullable: true),
+                    BarcodeType = table.Column<int>(type: "int", nullable: false),
+                    CurrencyId = table.Column<int>(type: "int", nullable: false),
+                    UnitId = table.Column<int>(type: "int", nullable: false),
+                    VatRateId = table.Column<int>(type: "int", nullable: false),
+                    NetPrice = table.Column<decimal>(type: "decimal(14,2)", precision: 14, scale: 2, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2(0)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2(0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Currencies_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalSchema: "definitions",
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Products_VatRates_VatRateId",
+                        column: x => x.VatRateId,
+                        principalSchema: "definitions",
+                        principalTable: "VatRates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CurrencyId",
+                schema: "products",
+                table: "Products",
+                column: "CurrencyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Name",
+                schema: "products",
+                table: "Products",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_VatRateId",
+                schema: "products",
+                table: "Products",
+                column: "VatRateId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products",
+                schema: "products");
+
+            migrationBuilder.DropTable(
+                name: "Currencies",
+                schema: "definitions");
+
+            migrationBuilder.DropTable(
+                name: "VatRates",
+                schema: "definitions");
+        }
+    }
+}
