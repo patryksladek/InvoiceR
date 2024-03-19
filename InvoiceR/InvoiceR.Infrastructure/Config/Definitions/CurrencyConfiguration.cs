42e3cf78a8d4858b693ac8fec103609bd5302973
patryk <patryksladek@wp.pl>
2024-03-19 21:37:22 +0100

    Added Definitions entity configurations

---
+++ InvoiceR/InvoiceR.Infrastructure/Config/Definitions/CurrencyConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using InvoiceR.Domain.Entities;
+using InvoiceR.Domain.Entities.Definitions;
+using InvoiceR.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InvoiceR.Infrastructure.Config.Definitions;
+
+public class CurrencyConfiguration : BaseEntityConfiguration<Currency>
+{
+    public override void Configure(EntityTypeBuilder<Currency> builder)
+    {
+        builder.ToTable("Currencies", "definitions");
+
+        builder.Property(x => x.Symbol)
+            .HasMaxLength(3)
+            .IsRequired();
+
+        builder.Property(x => x.Name)
+           .HasPrecision(240)
+           .IsRequired();
+
+        builder.Property(x => x.IsDefault)
+           .HasDefaultValue(false)
+           .IsRequired();
+
+        base.Configure(builder);
+    }
+}
---
+++ InvoiceR/InvoiceR.Infrastructure/Config/Definitions/VatRateConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using InvoiceR.Domain.Entities;
+using InvoiceR.Domain.Entities.Definitions;
+using InvoiceR.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InvoiceR.Infrastructure.Config.Definitions;
+
+public class VatRateConfiguration : BaseEntityConfiguration<VatRate>
+{
+    public override void Configure(EntityTypeBuilder<VatRate> builder)
+    {
+        builder.ToTable("VatRates", "definitions");
+
+        builder.Property(x => x.Symbol)
+            .HasMaxLength(12)
+            .IsRequired();
+
+        builder.Property(x => x.Value)
+           .HasPrecision(2)
+           .IsRequired();
+
+        base.Configure(builder);
+    }
+}
+
