64186b291692e41b5bd61766915ca605b80b293a
patryk <patryksladek@wp.pl>
2024-03-19 21:37:44 +0100

    Added DbSets to DbContext class

--- InvoiceR/InvoiceR.Infrastructure/Context/InvoicerDbContext.cs
+++ InvoiceR/InvoiceR.Infrastructure/Context/InvoicerDbContext.cs
@@ -1,5 +1,10 @@
 ﻿using InvoiceR.Domain.Entities.Customers;
+using InvoiceR.Domain.Entities.Definitions;
+using InvoiceR.Domain.Entities.Products;
 using InvoiceR.Infrastructure.Config.Customers;
+using InvoiceR.Infrastructure.Config.Definitions;
+using InvoiceR.Infrastructure.Config.Products;
+using MediatR;
 using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +14,9 @@ public class InvoicerDbContext : DbContext
 {
     public DbSet<Customer> Customers { get; set; }
     public DbSet<Country> Countries { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<VatRate> VatRates { get; set; }
+    public DbSet<Currency> Currencies { get; set; }
 
     public InvoicerDbContext(DbContextOptions options) : base(options)
     {
@@ -20,6 +28,11 @@ public class InvoicerDbContext : DbContext
         modelBuilder.ApplyConfiguration(new AddressConfiguration());
         modelBuilder.ApplyConfiguration(new ContactConfiguration());
         modelBuilder.ApplyConfiguration(new CountryConfiguration());
+
+        modelBuilder.ApplyConfiguration(new ProductConfiguration());
+
+        modelBuilder.ApplyConfiguration(new VatRateConfiguration());
+        modelBuilder.ApplyConfiguration(new CurrencyConfiguration());
 
         base.OnModelCreating(modelBuilder);
     }
