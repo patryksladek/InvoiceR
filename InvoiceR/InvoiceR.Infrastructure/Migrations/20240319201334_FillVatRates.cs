89ffb9c6093e970fca8018aab1514c2688ce4c14
patryk <patryksladek@wp.pl>
2024-03-19 21:38:56 +0100

    Added fill VatRates and Currencies scripts

---
+++ InvoiceR/InvoiceR.Infrastructure/Migrations/20240319201334_FillVatRates.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceR.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FillVatRates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO definitions.VatRates (Symbol, Value)
+                                    VALUES 
+                                    ('NP', 0.00),
+                                    ('ZW', 0.00),
+                                    ('7%', 0.07),
+                                    ('0%', 0.00),
+                                    ('5%', 0.05),
+                                    ('23%', 0.23),
+                                    ('8%', 0.08),
+                                    ('4%', 0.04),
+                                    ('-', 0.00)");
+        }
+
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE FROM definitions.VatRates");
+        }
+    }
+}
---
+++ InvoiceR/InvoiceR.Infrastructure/Migrations/20240319201529_FillCurrencies.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceR.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FillCurrencies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO definitions.Currencies (Symbol, Name, IsDefault)
+                                    VALUES 
+                                    ('PLN', 'Polish zloty', 1),
+                                    ('EUR', 'Euro', 0),
+                                    ('USD', 'U.S. Dollar', 0)");
+        }
+
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE FROM definitions.Currencies");
+        }
+    }
+}
