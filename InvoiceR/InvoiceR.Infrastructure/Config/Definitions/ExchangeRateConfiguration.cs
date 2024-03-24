fbd68907507836c9e880beb33b11a3892c9b40a5
patryk <patryksladek@wp.pl>
2024-03-24 10:41:57 +0100

    Added ExchangeRate entity configuration

---
+++ InvoiceR/InvoiceR.Infrastructure/Config/Definitions/ExchangeRateConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using InvoiceR.Domain.Entities.Definitions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InvoiceR.Infrastructure.Config.Definitions;
+
+public class ExchangeRateConfiguration : BaseEntityConfiguration<ExchangeRate>
+{
+    public override void Configure(EntityTypeBuilder<ExchangeRate> builder)
+    {
+        builder.ToTable("ExchangeRates", "definitions");
+
+        builder.HasOne(s => s.Currency)
+            .WithMany(g => g.ExchangeRates)
+            .HasForeignKey(s => s.CurrencyId);
+
+        builder.Property(x => x.Date)
+            .HasColumnType("datetime2(0)")
+            .IsRequired();
+
+        builder.Property(x => x.Rate)
+            .HasPrecision(16, 4)
+            .IsRequired();
+
+        builder.Property(x => x.TableNumber)
+          .HasMaxLength(240)
+          .IsRequired();
+
+        base.Configure(builder);
+    }
+}
