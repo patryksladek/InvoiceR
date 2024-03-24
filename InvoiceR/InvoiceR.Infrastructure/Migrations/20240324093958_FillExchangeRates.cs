9cdb8f06b46bf251fb8e68a27e0b38eaafd846f3
patryk <patryksladek@wp.pl>
2024-03-24 10:43:49 +0100

    Added ExchangeRate table migration and fill ExchangeRates scripts

---
+++ InvoiceR/InvoiceR.Infrastructure/Migrations/20240324093958_FillExchangeRates.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceR.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FillExchangeRates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO definitions.ExchangeRates (CurrencyId, Date, Rate, TableNumber)
+                                    VALUES 
+                                    (2, '2000-01-01 00:00:00.000', 4.30, ''),
+                                    (3, '2000-01-01 00:00:00.000', 4.00, '')");
+        }
+
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE FROM definitions.ExchangeRates");
+        }
+    }
+}
