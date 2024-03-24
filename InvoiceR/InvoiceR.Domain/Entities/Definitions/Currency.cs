bd60dc2d9ec09d11dbc8210d3eca22ed644ef291
patryk <patryksladek@wp.pl>
2024-03-24 10:41:36 +0100

    Added ExchangeRate entity

--- InvoiceR/InvoiceR.Domain/Entities/Definitions/Currency.cs
+++ InvoiceR/InvoiceR.Domain/Entities/Definitions/Currency.cs
@@ -7,5 +7,6 @@ public class Currency : BaseEntity
     public string Symbol { get; set; }
     public string Name { get; set; }
     public bool IsDefault { get; set; }
+    public ICollection<ExchangeRate> ExchangeRates { get; set; }
     public ICollection<Product> Products { get; set; }
 }
---
+++ InvoiceR/InvoiceR.Domain/Entities/Definitions/ExchangeRate.cs
@@ -0,0 +1,10 @@
+﻿namespace InvoiceR.Domain.Entities.Definitions;
+
+public class ExchangeRate : BaseEntity
+{
+    public int CurrencyId { get; set; }
+    public Currency Currency { get; set; }
+    public DateTime Date { get; set; }
+    public decimal Rate { get; set; }
+    public string TableNumber { get; set; }
+}
