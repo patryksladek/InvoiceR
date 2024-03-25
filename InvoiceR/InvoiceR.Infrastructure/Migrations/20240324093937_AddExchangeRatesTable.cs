b23fd8afc8d350fe3d79ee7c0ad76c8b14cb5a2b
patryk <patryksladek@wp.pl>
2024-03-25 08:50:46 +0100

    Added ExchangeRate table migration and fill ExchangeRates scripts

---
+++ InvoiceR/InvoiceR.Infrastructure/Migrations/20240324093937_AddExchangeRatesTable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceR.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExchangeRatesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExchangeRates",
+                schema: "definitions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CurrencyId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2(0)", nullable: false),
+                    Rate = table.Column<decimal>(type: "decimal(16,4)", precision: 16, scale: 4, nullable: false),
+                    TableNumber = table.Column<string>(type: "nvarchar(240)", maxLength: 240, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExchangeRates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ExchangeRates_Currencies_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalSchema: "definitions",
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExchangeRates_CurrencyId",
+                schema: "definitions",
+                table: "ExchangeRates",
+                column: "CurrencyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExchangeRates",
+                schema: "definitions");
+        }
+    }
+}
