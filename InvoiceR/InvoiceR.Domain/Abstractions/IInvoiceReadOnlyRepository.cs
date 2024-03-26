dd51158d67ea0fe842d43fc120b3c10e91f681f3
patryk <patryksladek@wp.pl>
2024-03-26 09:16:23 +0100

    Added data access abstraction for Invoice

---
+++ InvoiceR/InvoiceR.Domain/Abstractions/IInvoiceReadOnlyRepository.cs
@@ -0,0 +1,10 @@
+﻿using InvoiceR.Domain.Entities.Invoices;
+
+namespace InvoiceR.Domain.Abstractions;
+
+public interface IInvoiceReadOnlyRepository
+{
+    public IQueryable<Invoice> GetAllAsync();
+    public Task<Invoice> GetByIdWithDetailAsync(int id, CancellationToken cancellation = default);
+    public Task<InvoiceItem> GetInvoiceItemByIdWithDetailAsync(int itemId, CancellationToken cancellation = default);
+}
---
+++ InvoiceR/InvoiceR.Domain/Abstractions/IInvoiceRepository.cs
@@ -0,0 +1,11 @@
+﻿using InvoiceR.Domain.Entities.Invoices;
+
+namespace InvoiceR.Domain.Abstractions;
+public interface IInvoiceRepository
+{
+    Task<Invoice> GetByIdAsync(int id, CancellationToken cancellation = default);
+    Task<string> GetNextInvoiceNumberAsync(CancellationToken cancellation = default);
+    void Add(Invoice product);
+    void Update(Invoice product);
+    void Delete(Invoice product);
+}
