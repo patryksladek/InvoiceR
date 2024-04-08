787a4f63b77774eb66c9650b9fe13c657d37efa2
patryk <patryksladek@wp.pl>
2024-04-08 22:11:08 +0200

    Added unit tests to GetCurrenciesQueryHandler

---
+++ InvoiceR/InvoiceR.UnitTests/Queries/Currencies/GetCurrencies/GetCurrenciesQueryHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using InvoiceR.Application.Queries.Currencies.GetCurrencies;
+using InvoiceR.Domain.Abstractions;
+using InvoiceR.Domain.Entities.Definitions;
+using Moq;
+
+namespace InvoiceR.UnitTests.Queries.Currencies.GetCurrencies;
+
+public class GetCurrenciesQueryHandlerTests
+{
+    private readonly Mock<ICurrencyReadOnlyRepository> _currencyReadOnlyRepository;
+
+    public GetCurrenciesQueryHandlerTests()
+    {
+        _currencyReadOnlyRepository = new();
+    }
+
+    [Fact]
+    public void Handle_Should_CallGetAllAsyncOnRepository_WhenGetCurrenciesQuery()
+    {
+        // Arrange
+        var currenciesQuery = new List<Currency>().AsQueryable();
+
+        _currencyReadOnlyRepository.Setup(
+            x => x.GetAllAsync()).Returns(currenciesQuery);
+
+        var handler = new GetCurrenciesQueryHandler(
+            _currencyReadOnlyRepository.Object);
+
+        // Act
+        handler.Handle(new GetCurrenciesQuery(), default);
+
+        // Assert
+        _currencyReadOnlyRepository.Verify(x => x.GetAllAsync(), Times.Once);
+    }
+}
