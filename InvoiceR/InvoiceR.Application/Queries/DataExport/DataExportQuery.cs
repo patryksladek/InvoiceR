5bb3dd0f0fd9a41d12bc785278e34ea6f80f8f17
patryk <patryksladek@wp.pl>
2024-04-10 23:37:02 +0200

    Added query DataExport (template)

---
+++ InvoiceR/InvoiceR.Application/Queries/DataExport/DataExportQuery.cs
@@ -0,0 +1,6 @@
+﻿using InvoiceR.Application.Configuration.Queries;
+using System.Collections;
+
+namespace InvoiceR.Application.Queries.DataExport;
+
+public record DataExportQuery(IList Data, string FilePath) : IQuery<byte[]>;
---
+++ InvoiceR/InvoiceR.Application/Queries/DataExport/DataExportQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using InvoiceR.Application.Configuration.Queries;
+using InvoiceR.Domain.Abstractions.DataExporter;
+using InvoiceR.Domain.Exceptions;
+
+namespace InvoiceR.Application.Queries.DataExport;
+
+internal class DataExportQueryHandler : IQueryHandler<DataExportQuery, byte[]>
+{
+    private readonly IExportStrategy _exportStrategy;
+
+    public DataExportQueryHandler(IExportStrategy dataGenerator)
+    {
+        _exportStrategy = dataGenerator;
+    }
+
+    public async Task<byte[]> Handle(DataExportQuery request, CancellationToken cancellationToken)
+    {
+        return null;
+    }
+}
