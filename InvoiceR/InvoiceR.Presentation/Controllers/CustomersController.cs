061be51e1f397634b8be71ea0b594b335c6c1fe7
patryk <patryksladek@wp.pl>
2024-04-15 09:02:37 +0200

    Added Export Customers action

--- InvoiceR/InvoiceR.Presentation/Controllers/CustomersController.cs
+++ InvoiceR/InvoiceR.Presentation/Controllers/CustomersController.cs
@@ -2,12 +2,14 @@
 using InvoiceR.Application.Commands.Customers.EditCusotmer;
 using InvoiceR.Application.Commands.Customers.RemoveCustomer;
 using InvoiceR.Application.Dto;
+using InvoiceR.Application.Queries.Customers.ExportCustomers;
 using InvoiceR.Application.Queries.Customers.GetCustomerById;
 using InvoiceR.Application.Queries.Customers.GetCustomers;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 using System.Net;
+using System.Net.Mime;
 
 namespace InvoiceR.Presentation.Controllers;
 
@@ -38,6 +40,15 @@ public class CustomersController : Controller
     {
         var result = await _mediator.Send(new GetCustomerByIdQuery(id));
         return result != null ? Ok(result) : NotFound();
+    }
+
+    [HttpGet("{exportType}/{ids}")]
+    [SwaggerOperation("Export Customers")]
+    [ProducesResponseType((int)HttpStatusCode.NoContent)]
+    public async Task<ActionResult> Export([FromRoute] ExportTypeDto exportType, [FromRoute] int[] ids)
+    {
+        var result = await _mediator.Send(new ExportCustomersQuery(ids, exportType));
+        return File(result.Content, MediaTypeNames.Application.Octet, result.Name);
     }
 
     [HttpPost]
