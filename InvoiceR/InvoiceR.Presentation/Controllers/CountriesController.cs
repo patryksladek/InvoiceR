de1d80c54e95cee229781605f44c92afa7596f10
patryk <patryksladek@wp.pl>
2024-04-19 20:42:16 +0200

    Added controller to get countries

---
+++ InvoiceR/InvoiceR.Presentation/Controllers/CountriesController.cs
@@ -0,0 +1,36 @@
+﻿using InvoiceR.Application.Commands.Customers.AddCustomer;
+using InvoiceR.Application.Commands.Customers.EditCusotmer;
+using InvoiceR.Application.Commands.Customers.RemoveCustomer;
+using InvoiceR.Application.Dto;
+using InvoiceR.Application.Queries.Countries.GetCountries;
+using InvoiceR.Application.Queries.Customers.ExportCustomers;
+using InvoiceR.Application.Queries.Customers.GetCustomerById;
+using InvoiceR.Application.Queries.Customers.GetCustomers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
+using System.Net.Mime;
+
+namespace InvoiceR.Presentation.Controllers;
+
+[Route("api/Countries")]
+[ApiController]
+public class CountriesController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public CountriesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [SwaggerOperation("Get Countries")]
+    [ProducesResponseType(typeof(IEnumerable<CountryDto>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult> Get()
+    {
+        var result = await _mediator.Send(new GetCountriesQuery());
+        return Ok(result);
+    }
+}
