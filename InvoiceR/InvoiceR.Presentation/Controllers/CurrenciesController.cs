cfbab53a6787e27884680089cc62043826f309ee
patryk <patryksladek@wp.pl>
2024-04-20 08:07:14 +0200

    Added controller to get currencies

---
+++ InvoiceR/InvoiceR.Presentation/Controllers/CurrenciesController.cs
@@ -0,0 +1,30 @@
+﻿using InvoiceR.Application.Dto;
+using InvoiceR.Application.Queries.Countries.GetCountries;
+using InvoiceR.Application.Queries.Currencies.GetCurrencies;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
+
+namespace InvoiceR.Presentation.Controllers;
+
+[Route("api/Currencies")]
+[ApiController]
+public class CurrenciesController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public CurrenciesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [SwaggerOperation("Get Currencies")]
+    [ProducesResponseType(typeof(IEnumerable<CountryDto>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult> Get()
+    {
+        var result = await _mediator.Send(new GetCurrenciesQuery());
+        return Ok(result);
+    }
+}
