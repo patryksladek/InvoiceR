48cdc59250f0ada8bd195eaf1e2fbf3a2e22f911
patryk <patryksladek@wp.pl>
2024-04-21 15:21:08 +0200

    Added controller to get units

---
+++ InvoiceR/InvoiceR.Presentation/Controllers/UnitsController.cs
@@ -0,0 +1,31 @@
+﻿using InvoiceR.Application.Dto;
+using InvoiceR.Application.Queries.Currencies.GetCurrencies;
+using InvoiceR.Application.Queries.Units.GetUnits;
+using InvoiceR.Application.Queries.VatRates.GetVatRates;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
+
+namespace InvoiceR.Presentation.Controllers;
+
+[Route("api/Units")]
+[ApiController]
+public class UnitsController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public UnitsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [SwaggerOperation("Get Units")]
+    [ProducesResponseType(typeof(IEnumerable<UnitDto>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult> Get()
+    {
+        var result = await _mediator.Send(new GetUnitsQuery());
+        return Ok(result);
+    }
+}
