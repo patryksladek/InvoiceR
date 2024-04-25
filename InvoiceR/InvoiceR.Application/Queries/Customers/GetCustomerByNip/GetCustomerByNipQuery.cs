0281f1c819b102792fc3bf166a1a3a531708550d
patryk <patryksladek@wp.pl>
2024-04-25 22:06:46 +0200

    Added query GetCustomerByNip

---
+++ InvoiceR/InvoiceR.Application/Queries/Customers/GetCustomerByNip/GetCustomerByNipQuery.cs
@@ -0,0 +1,6 @@
+﻿using InvoiceR.Application.Configuration.Queries;
+using InvoiceR.Application.Dto;
+
+namespace InvoiceR.Application.Queries.Customers.GetCustomerByNip;
+
+public record GetCustomerByNipQuery(string nip) : IQuery<CustomerDetailDto>;
---
+++ InvoiceR/InvoiceR.Application/Queries/Customers/GetCustomerByNip/GetCustomerByNipQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using InvoiceR.Application.Configuration.Queries;
+using InvoiceR.Application.Dto;
+using InvoiceR.Domain.Abstractions;
+using Mapster;
+
+namespace InvoiceR.Application.Queries.Customers.GetCustomerByNip;
+
+internal class GetCustomerByNipQueryHandler : IQueryHandler<GetCustomerByNipQuery, CustomerDetailDto>
+{
+    private readonly IGusService _gusService;
+
+    public GetCustomerByNipQueryHandler(IGusService gusService)
+    {
+        _gusService = gusService;
+    }
+
+    public async Task<CustomerDetailDto> Handle(GetCustomerByNipQuery request, CancellationToken cancellationToken)
+    {
+        string searchResult = await _gusService.GetSearchResultByNipAsync(request.nip);
+
+        return null;
+    }
+}
