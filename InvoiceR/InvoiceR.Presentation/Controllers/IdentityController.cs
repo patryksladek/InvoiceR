35bc67be2a388511f0512eb5a4c7cd3ed708b0fe
patryk <patryksladek@wp.pl>
2024-05-02 20:15:50 +0200

    Add Identity controller

---
+++ InvoiceR/InvoiceR.Presentation/Controllers/IdentityController.cs
@@ -0,0 +1,29 @@
+﻿using InvoiceR.Application.Commands.Customers.AddCustomer;
+using InvoiceR.Application.Commands.Identity.RegisterUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
+
+namespace InvoiceR.Presentation.Controllers;
+
+[Route("api/Identity")]
+[ApiController]
+public class IdentityController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public IdentityController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    [Route("Register User")]
+    [ProducesResponseType((int)HttpStatusCode.Created)]
+    public async Task<ActionResult> Post([FromBody] RegisterUserCommand command)
+    {
+        await _mediator.Send(command);
+        return Created();
+    }
+}
